3de07bec1bcacc6929689ac4ba200701372a925a
jypma <jan@ypmania.nl>
2013-09-04 21:29:42 +0200

    write some tests

--- src/test/scala/net/ypmania/lore/PagedFileSpec.scala
+++ src/test/scala/net/ypmania/lore/PagedFileSpec.scala
@@ -18,18 +18,16 @@ import akka.testkit.ImplicitSender
 
 class PagedFileSpec extends TestKit(ActorSystem("Test")) with ImplicitSender 
                        with WordSpec with ShouldMatchers with Eventually {
-  class Fixture  {
+  class Fixture(val initialPages:Int = 0, val initialJournalIndex:Vector[PageIdx] = Vector.empty[PageIdx])  {
     val dataFile = TestProbe()
     val journalFile = TestProbe()
     val dataHeader = PagedFile.DataHeader()
     val journalHeader = PagedFile.JournalHeader()
-    val initialJournalIndex = Vector.empty[PageIdx]
-    val initialPages = PageIdx(0)
     val content = ByteString("Hello, world")
     val pageContent = content ++ ByteString(new Array[Byte](dataHeader.pageSize - content.size))
     
     val f = TestActorRef(PagedFile.props(dataFile.ref, journalFile.ref, dataHeader,
-        journalHeader, initialJournalIndex, initialPages))
+        journalHeader, initialJournalIndex, PageIdx(initialPages)))
   }
   
   "a paged file" should {
@@ -50,7 +48,7 @@ class PagedFileSpec extends TestKit(ActorSystem("Test")) with ImplicitSender
       val haswritten = expectMsgType[PagedFile.WriteCompleted]
     }
     
-    "return content if it is stored in the journal" in new Fixture {
+    "return content after it is stored in the journal" in new Fixture {
       f ! PagedFile.Write(PageIdx(0), content)
       val write = journalFile.expectMsgType[FileActor.Write]
       // page 0 => right after the header
@@ -68,8 +66,30 @@ class PagedFileSpec extends TestKit(ActorSystem("Test")) with ImplicitSender
       hasread.content should be (pageContent)
     }
     
-    "return content if it is stored in the data" in new Fixture {
+    "return content if it is stored in the journal" in new Fixture(
+        initialPages = 1,
+        initialJournalIndex = Vector(PageIdx(0))) {
       
+      f ! PagedFile.Read(PageIdx(0))
+      val read = journalFile.expectMsgType[FileActor.Read]
+      // prefixed with page number
+      read.from should be (PagedFile.JournalHeader.size + 4) 
+      read.size should be (dataHeader.pageSize)
+      journalFile.reply(FileActor.ReadCompleted(pageContent, read.ctx))
+      val hasread = expectMsgType[PagedFile.ReadCompleted]
+      hasread.content should be (pageContent)      
+    }
+    
+    "return content if it is stored in the data" in new Fixture(
+        initialPages = 1) {
+      
+      f ! PagedFile.Read(PageIdx(0))
+      val read = dataFile.expectMsgType[FileActor.Read]
+      read.from should be (PagedFile.DataHeader.size)
+      read.size should be (dataHeader.pageSize)
+      dataFile.reply(FileActor.ReadCompleted(pageContent, read.ctx))
+      val hasread = expectMsgType[PagedFile.ReadCompleted]
+      hasread.content should be (pageContent)            
     }
     
     "put writes into consecutive journal pages" in new Fixture {
